61c8cf1491bb9cb60bdae4d56af577104bec2194
Timothy <knbtimothy@gmail.com>
2025-06-22 11:19:54 +0100

    added another layer of api

---
+++ stamp-chain/src/app/test/page.tsx
@@ -0,0 +1,19 @@
+
+const page = async () => {
+    const fetchData = async () => {
+        const api = await fetch('http://stampchain-eight.vercel.app/api/test');
+        const res = await api.json();
+        return res
+    }
+
+    const data = await fetchData()
+
+  return (
+    <div>
+       <h1> page</h1>
+       <h2>The data is: {JSON.stringify(data)}</h2>
+    </div>
+  )
+}
+
+export default page
