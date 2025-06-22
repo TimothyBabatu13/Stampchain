aa9c658159b66195039f93faf98c26585027e6eb
Timothy <knbtimothy@gmail.com>
2025-06-22 11:23:16 +0100

    ,

--- stamp-chain/src/app/test/page.tsx
+++ stamp-chain/src/app/test/page.tsx
@@ -1,17 +1,17 @@
 
 const page = async () => {
-    // const fetchData = async () => {
-    //     const api = await fetch('http://stampchain-eight.vercel.app/api/test');
-    //     const res = await api.json();
-    //     return res
-    // }
+    const fetchData = async () => {
+        const api = await fetch('http://stampchain-eight.vercel.app/api/test');
+        const res = await api.json();
+        return res
+    }
 
-    // const data = await fetchData()
+    const data = await fetchData()
 
   return (
     <div>
        <h1> page</h1>
-       {/* <h2>The data is: {JSON.stringify(data)}</h2> */}
+       <h2>The data is: {JSON.stringify(data)}</h2>
     </div>
   )
 }
