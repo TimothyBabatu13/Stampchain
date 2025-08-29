ee60c1ab817f9f76b400b4baa6a275f8d8785044
Timothy <knbtimothy@gmail.com>
2025-08-29 14:07:49 +0100

    worked on the (admin) page.tsx file to show the right loading shimmer

--- stamp-chain/src/app/(protected-route)/admin/components/view-campaign-skeleton.tsx
+++ stamp-chain/src/app/(protected-route)/admin/components/view-campaign-skeleton.tsx
@@ -3,11 +3,12 @@ import { Skeleton } from "@/components/ui/skeleton"
 const ViewCampaignSkeleton = () => {
   return (
     <div>
-        <Skeleton className="h-[250px] bg-card rounded-xl border p-6 shadow-sm grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
-            <Skeleton className="h-[100px] "/>
-            <Skeleton className="h-[100px] "/>
-            <Skeleton className="h-[100px] "/>
-        </Skeleton>
+        <div className="bg-card rounded-xl border p-6 shadow-sm grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+            <Skeleton className="h-[100px] bg-[#003]"/>
+            <Skeleton className="h-[100px] bg-[#003]"/>
+            <Skeleton className="h-[100px] bg-[#003]"/>
+            <Skeleton className="h-[100px] bg-[#003]"/>
+        </div>
     </div>
   )
 }
